75c56762d36ff0179f1e0e5bbfa92956a3ba87e7
murisdotcom <muhrizalsidik@gmail.com>
2022-02-18 20:30:10 +0700

    Merubah routes & Menambahkan controller users untuk admin

---
+++ app/Controllers/Admin/Users.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Users extends BaseController
+{
+    public function index()
+    {
+        echo "Ini controller Users method index yang ada dalam control admin";
+    }
+}
