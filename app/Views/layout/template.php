7621e755149a963beed441ca8e082646d519a885
murisdotcom <muhrizalsidik@gmail.com>
2022-02-18 22:58:06 +0700

    Memisahkan navbar dari file template atau bisa disebut parsial

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -15,30 +15,7 @@
 
 <body>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">MS GLOW</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="<?= base_url('/'); ?>">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="<?= base_url('/pages/about'); ?>">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/pages/contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </nav>
+  <?= $this->include('layout/navbar'); ?>
 
   <?= $this->renderSection('content'); ?>
 
