c16fd78f68104347ff8a22bb7b79e72d38a7d9cc
murisdotcom <muhrizalsidik@gmail.com>
2022-02-18 22:10:12 +0700

    Menambahkan variabel data untuk title setiap halamannya biar beda

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -7,12 +7,25 @@ class Pages extends BaseController
 {
   public function index()
   {
-    return view('pages/home');
+
+    $data = [
+      'title' => 'Home | Website MS GLOW'
+    ];
+
+    echo view('layout/header', $data);
+    echo view('pages/home');
+    echo view('layout/footer');
   }
 
   public function about()
   {
-    return view('pages/about');
+        $data = [
+      'title' => 'About Me | Website MS GLOW'
+    ];
+
+    echo view('layout/header', $data);
+    echo view('pages/about');
+    echo view('layout/footer');
   }
   
 }
