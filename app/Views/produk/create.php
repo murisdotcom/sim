255215e84dc76343f43824de1b62e86cd95db11e
murisdotcom <muhrizalsidik@gmail.com>
2022-02-21 20:25:33 +0700

    Menambahkan <?= csrf_field(); ?> di file create.php agar lebih aman, jadi hanya bisa diinput di halaman tsb

--- app/Views/produk/create.php
+++ app/Views/produk/create.php
@@ -5,27 +5,28 @@
   <div class="row">
     <div class="col-8">
       <h2 class="my-3">Form Tambah Data Produk</h2>
-      <form>
-        <div class="row mb-3">
+      <?= csrf_field(); ?>
+      <form action="produk/save" method="post">
+        <div class="form-group row mb-3">
           <label for="nama_produk" class="col-sm-2 col-form-label">Nama Produk</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="nama_produk" name="nama_produk">
+            <input type="text" class="form-control" id="nama_produk" name="nama_produk" autofocus>
           </div>
         </div>
-        <div class="row mb-3">
+        <div class="form-group row mb-3">
           <label for="desc_produk" class="col-sm-2 col-form-label">Kegunaan</label>
           <div class="col-sm-10">
             <!-- <input type="text" class="form-control" id="desc_produk"> -->
-            <textarea class="form-control"></textarea>
+            <textarea class="form-control" id="desc_produk"></textarea>
           </div>
         </div>
-        <div class="row mb-3">
+        <div class="form-group row mb-3">
           <label for="kode_produk" class="col-sm-2 col-form-label">Kode Produk</label>
           <div class="col-sm-10">
             <input type="text" class="form-control" id="kode_produk">
           </div>
         </div>
-        <div class="row mb-3">
+        <div class="form-group row mb-3">
           <label for="gambar" class="col-sm-2 col-form-label">Gambar</label>
           <div class="col-sm-10">
             <input type="text" class="form-control" id="gambar">
